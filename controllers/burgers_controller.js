9b5ff684cb21c67e1630c0b7097d2fb0e4a06516
Chuck Wessel <chuck.wessel@gmail.com>
2018-06-21 18:11:43 -0500

    Add Heroku JawsDB connection profile

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,21 +6,21 @@ var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
     burger.all(function(burger_data){
-        console.log(burger_data);
+        // console.log(burger_data);
         res.render("index.handlebars",{burger_data});
     })
 })
 
 router.post("/burgers/update", function(req, res){
     burger.update(req.body.burger_id, function(result){
-        console.log(result);
+        // console.log(result);
         res.redirect("/");
     })
 })
 
 router.post("/burgers/create", function(req, res){
     burger.create(req.body.burger_name, function(result){
-        console.log(result);
+        // console.log(result);
         res.redirect("/");
     })
 })
